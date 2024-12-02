565a7b16fb722c6fff409ae646af13b5f65f7e47
FereWulf <finn@toxicflames.co.uk>
2024-12-02 05:07:56 +0000

    Assigned RGB values to volume levels

--- led-app.py
+++ led-app.py
@@ -4,15 +4,36 @@ import struct
 import math
 import sounddevice as sd
 
-n = 0
+def volume_to_rgb(volume):
+	degree = volume * 1.8  #200 * 1.8 = 360
+
+	arc = math.floor(degree / 120)
+
+	r = 0
+	g = 0
+	b = 0
+
+	if (arc == 0):
+		r = 1 - (degree / 120) - 0.1
+		b = degree / 120 + 0.1
+	elif (arc == 1):
+		b = 1 - (degree / 120 - 1)
+		g = degree / 120 - 1
+	else:
+		g = 1 - (degree / 120 - 2)
+		r = degree / 120 - 2
+
+	r = round(float(np.clip(r * 255, 0, 255)), 2)
+	g = round(float(np.clip(g * 255, 0, 255)), 2)
+	b = round(float(np.clip(b * 255, 0, 255)), 2)
+
+	return r, g, b
 
 def audio_callback(indata, frames, time, status):
-	global n
-
 	volume_norm = np.linalg.norm(indata) * 10
 	volume = np.clip(int(volume_norm), 0, 200)
 
-	print(volume)
+	print(volume, volume_to_rgb(volume))
 	
 def getAudio(timer):
 	
